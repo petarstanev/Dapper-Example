38a19a23aebca794a3fdda7374581493f2334ec6
Petar Stanev <petarstanev@outlook.com>
2016-08-21 15:26:47 +0100

    Init

---
+++ DapperASP/Controllers/DapperController.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using DapperASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DapperASP.Controllers
+{
+    public class DapperController : Controller
+    {
+        // GET: Dapper
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public String Index()
+        {
+            string output = "";
+
+            string ConnectionString = ConfigurationManager.ConnectionStrings["DapperConnectionString"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var persons = connection.Query<User>("Select * FROM Users").ToList();
+
+                foreach (User person in persons)
+                {
+                    output += person.username;
+                }
+
+
+            }
+
+            return output;
+        }
+    }
+}
